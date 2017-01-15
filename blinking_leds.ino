5614a7618c9c3fc7acda1bcbc72565393f2d9aed
soziologikus <mail@wernerreichmann.net>
2017-01-15 21:56:47 +0100

    Update blinking_leds.ino

--- blinking_leds.ino
+++ blinking_leds.ino
@@ -1,8 +1,6 @@
-int i = 150;
+int i = 300;
 
-int ledNr = 2;
 
-int durchgang = 1;
 
 
 void setup() {            // initialize digital pin LED_BUILTIN as an output.
@@ -13,14 +11,57 @@ void setup() {            // initialize digital pin LED_BUILTIN as an output.
   pinMode(6, OUTPUT);
 }
 
-// the loop function runs over and over again forever
 void loop() {
 
-  digitalWrite(2, LOW);   // turn the LED offby making the voltage LOW
-  digitalWrite(3, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(4, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(5, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(6, LOW);    // turn the LED off by making the voltage LOW
+kitt();
+alle_blinken();
+
+for (int y=1; y<3; y++){
+anna();
+}
+
+alle_blinken();
+
+}
+
+
+void anna() {
+
+int phase = 1;
+
+for (int g = 1; g < 5; g++){
+  
+  digitalWrite(2, LOW);   
+  digitalWrite(3, LOW);    
+  digitalWrite(4, LOW);    
+  digitalWrite(5, LOW);   
+  digitalWrite(6, LOW);    
+
+if (g == 1){digitalWrite(4, HIGH);}
+if (g == 2){digitalWrite(3, HIGH); digitalWrite(5, HIGH);}
+if (g == 3){digitalWrite(2, HIGH); digitalWrite(6, HIGH);}
+if (g == 4){digitalWrite(3, HIGH); digitalWrite(5, HIGH);}
+
+delay(i*1.5);  
+
+}
+  
+}
+
+
+void kitt() {
+
+int ledNr = 2;
+
+int durchgang = 1;
+
+for(int c=1; c < 18; c++){
+
+  digitalWrite(2, LOW);   
+  digitalWrite(3, LOW);    
+  digitalWrite(4, LOW);    
+  digitalWrite(5, LOW);    
+  digitalWrite(6, LOW);    
 
   digitalWrite(ledNr, HIGH);
 
@@ -47,7 +88,36 @@ if (durchgang == 2) {
 
 if (ledNr == 2) { durchgang = 1; }
 
-
+}
 
 }
+
+
+
+void alle_blinken() {
+
+for(int x=1; x<4; x++){
+
+  digitalWrite(2, LOW);   
+  digitalWrite(3, LOW);   
+  digitalWrite(4, LOW);    
+  digitalWrite(5, LOW);   
+  digitalWrite(6, LOW);    
+  
+  delay(i*2);
+
+  digitalWrite(2, HIGH);   
+  digitalWrite(3, HIGH);   
+  digitalWrite(4, HIGH);   
+  digitalWrite(5, HIGH);     
+  digitalWrite(6, HIGH);
+
+  delay(i*2);
+    
+}
+
+}
+  
+
+
 
