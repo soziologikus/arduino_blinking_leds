b933840fab67c9433f55acaa6625e95173b30dd7
soziologikus <mail@wernerreichmann.net>
2017-01-16 21:58:53 +0100

    Update blinking_leds.ino

--- blinking_leds.ino
+++ blinking_leds.ino
@@ -6,6 +6,7 @@ void setup() {
   pinMode(4, OUTPUT);
   pinMode(5, OUTPUT);
   pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
 }
 
 
@@ -19,6 +20,30 @@ for (int y=1; y<5; y++){anna();}
 
 alle_blinken();
 
+blinki();
+
+}
+
+void blinki() {
+for (int z = 1; z < 13; z++){
+  
+  digitalWrite(2, LOW);   
+  digitalWrite(3, LOW);    
+  digitalWrite(4, LOW);    
+  digitalWrite(5, LOW);   
+  digitalWrite(6, LOW);
+
+digitalWrite(2, HIGH); digitalWrite(3, HIGH); digitalWrite(5, HIGH); digitalWrite(6, HIGH);
+
+delay(i);
+
+digitalWrite(2, LOW); digitalWrite(3, LOW); digitalWrite(5, LOW); digitalWrite(6, LOW);
+digitalWrite(4, HIGH);
+
+delay(i);
+
+}
+  
 }
 
 
@@ -89,7 +114,8 @@ for(int x=1; x<4; x++){
   digitalWrite(3, LOW);   
   digitalWrite(4, LOW);    
   digitalWrite(5, LOW);   
-  digitalWrite(6, LOW);    
+  digitalWrite(6, LOW);   
+  digitalWrite(7, LOW); 
   
   delay(i*2);
 
@@ -98,9 +124,16 @@ for(int x=1; x<4; x++){
   digitalWrite(4, HIGH);   
   digitalWrite(5, HIGH);     
   digitalWrite(6, HIGH);
+  digitalWrite(7, HIGH);
 
   delay(i*2);
+
+  digitalWrite(7, LOW);
     
 }
 
 }
+  
+
+
+
